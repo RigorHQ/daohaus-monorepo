8c7458fb9759bd73617779859c11b2c4aea9a873
dan13ram <10012209+dan13ram@users.noreply.github.com>
2023-08-29 00:57:13 +0530

    using custom subgraph for goerli

--- libs/keychain-utils/src/endpoints.ts
+++ libs/keychain-utils/src/endpoints.ts
@@ -4,7 +4,7 @@ export const ENDPOINTS: KeychainList = {
   V3_SUBGRAPH: {
     '0x1':
       'https://gateway.thegraph.com/api/[api-key]/subgraphs/id/GfHFdFmiSwW1PKtnDhhcxhArwtTjVuMnXxQ5XcETF1bP',
-    '0x5': 'https://api.thegraph.com/subgraphs/name/hausdao/daohaus-v3-goerli',
+    '0x5': 'https://api.thegraph.com/subgraphs/name/dan13ram/daohaus-v3-goerli',
     '0x64':
       'https://gateway.thegraph.com/api/[api-key]/subgraphs/id/5oXHJGgAWTSEXHK5FGf7mcxYtk6rSz7MJV5ccvGfoW6q',
     '0x89':
